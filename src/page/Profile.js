f1f6469069ecb4973b150030899b4d70143890c7
zaymeboy <azamspm1057@gmail.com>
2024-09-09 16:15:38 +0800

    Profile page update

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -4,16 +4,17 @@ import BoxCompo from './Component/BoxCompo'
 
 function Profile() { 
     const outside=[
-        "Full Name", "IC Number" , "Matrix or Staff Number"
+        "Full Name", "IC Number" , "Matrix or Staff Number" , "Department" , "Email"
     ];
     const inside=[
-        "MOHAMMAD AZAM BIN RAMLI", "041101121057" , "05DDT22F1070"
+        "MOHAMMAD AZAM BIN RAMLI", "041101121057" , "05DDT22F1070" , "JTMK" , "Azamspm1057@gmail.com"
     ];
   return (
     <div className='box_profile'>
         {outside.map((a , i)=>(
             <BoxCompo label={a} fill={inside[i]}/>
         ))}
+        <button>Update</button>
     </div>
   )
 }
